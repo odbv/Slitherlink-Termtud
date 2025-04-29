f227f4364619d337dc9455475b81e550e5779a10
odbv <orbandavid200612@gmail.com>
2025-04-29 20:15:20 +0300

    python-sat init/test

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-from pysat.solvers import Glucose3
+from pysat.solvers import Glucose42
 
-g = Glucose3
+g = Glucose42()
 g.add_clause([-1, 2])
 g.add_clause([-2, 3])
 print(g.solve())
+print(g.get_model())
